0d2434addc107338bc740ee5b4b0a301275cca6a
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-02-07 10:50:56 +0200

    Time: 85 ms (81.25%), Space: 38.3 MB (9.38%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode BstToGst(TreeNode root) {
+        SUM(root);
+        return root;
+
+    }
+    int sum=0;
+    public int SUM(TreeNode node){
+        if(node== null) 
+            return 0;
+        SUM(node.right);
+        
+        Console.WriteLine($"visit {node.val}");
+        sum+= node.val;
+        node.val = sum;
+
+        SUM(node.left);
+        
+        return  node.val;
+    }
+}
