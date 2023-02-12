cf5b5780543c0e3e715b27a6db3d6587e6876b24
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-02-12 13:01:40 +0200

    Time: 114 ms (26.96%), Space: 40.8 MB (52.46%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool CheckIfExist(int[] arr) {
+        int n =arr.Length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(arr[i] == 2*arr[j]) return true;
+            }
+        }
+        return false;
+    }
+}
