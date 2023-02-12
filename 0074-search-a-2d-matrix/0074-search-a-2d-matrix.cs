d6959bfb2391e11c4156bc2dad90ce0350ae6eed
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-02-12 10:50:14 +0200

    Time: 99 ms (74.77%), Space: 40.6 MB (60.26%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int n =matrix.Length;
+        int m = matrix[0].Length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++)
+                if( matrix[i][j] == target)return true;
+        }
+        return false;
+    }
+}
