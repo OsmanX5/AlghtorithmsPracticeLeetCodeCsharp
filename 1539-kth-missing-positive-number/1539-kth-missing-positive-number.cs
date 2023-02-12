484f1c74ba40fd81a2af61f0ecc7175119b22c45
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-02-12 11:40:17 +0200

    Time: 292 ms (5.34%), Space: 43.2 MB (6.76%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cs
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cs
@@ -2,11 +2,9 @@ public class Solution {
     public int FindKthPositive(int[] arr, int k) {
         HashSet<int> all = new();
         for(int i=1;i<arr.Max() +k+1;i++)all.Add(i);
-        foreach(int x in all)Console.Write($" {x} ");
         Console.WriteLine();
         for(int i=0;i<arr.Length;i++)all.Remove(arr[i]);
         int[] deleted= all.ToArray();
-        foreach(int x in deleted)Console.Write($" {x} ");
         return deleted[k-1];
     }
 }
