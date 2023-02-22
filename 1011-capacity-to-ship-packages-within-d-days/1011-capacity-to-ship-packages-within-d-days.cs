7cae15784815f3a9e2a64d3be1c07c4a67ecca39
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-02-22 06:14:00 +0200

    Time: 1053 ms (5.26%), Space: 48.5 MB (15.79%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cs
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cs
@@ -2,24 +2,26 @@ public class Solution {
     public int ShipWithinDays(int[] weights, int days) {
         int l = 0;
         int r = 0;
-        for(int i=0;i<weights.Length;i++){
+        int n=weights.Length;
+        for(int i=0;i<n;i++){
             if(weights[i]>l) l =weights[i];
             r+=weights[i];
         }
         while(l<r){
             int mid = l+(r-l)/2;
-            int d = GetDays(weights,mid);
+            bool d = GetDays(weights,mid,days);
            // Console.WriteLine($"l ={l} , mid = {mid}  r ={r} , days ={d}");
-            if(d<=days) r = mid;
+            if(d) r = mid;
             else l+=1;
         }
         return l;
         
     }
-    int GetDays(int[] nums,int maxWeight){
+    bool GetDays(int[] nums,int maxWeight,int daysCount){
         int days =1;
         int sum=0;
-        for(int i=0;i<nums.Length;i++){
+        int n= nums.Length;
+        for(int i=0;i<n;i++){
             if(sum + nums[i]<=maxWeight)
                 sum+=nums[i];
             else{
@@ -27,6 +29,6 @@ public class Solution {
                 sum =nums[i];
             }
         }
-        return days;
+        return days<=daysCount;
     }
 }
