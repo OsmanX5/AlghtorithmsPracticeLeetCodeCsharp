1d13332566478acff19ee426ea17755a273d0c2d
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-02-22 06:07:32 +0200

    Time: 1373 ms (5.26%), Space: 53.4 MB (5.26%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int ShipWithinDays(int[] weights, int days) {
+        int l = weights.Max();
+        int r = weights.Sum();
+        
+        while(l<r){
+            
+            int mid = l+(r-l)/2;
+            int d = GetDays(weights,mid);
+            Console.WriteLine($"l ={l} , mid = {mid}  r ={r} , days ={d}");
+            if(d<=days) r = mid;
+            else l+=1;
+        }
+        return l;
+        
+    }
+    int GetDays(int[] nums,int maxWeight){
+        int days =1;
+        int sum=0;
+        for(int i=0;i<nums.Length;i++){
+            if(sum + nums[i]<=maxWeight)
+                sum+=nums[i];
+            else{
+                days++;
+                sum =nums[i];
+            }
+        }
+        return days;
+    }
+}
