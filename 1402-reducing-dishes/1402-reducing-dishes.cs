ec80106200798c22c3a277acec0b0d526999ef4c
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-03-29 07:48:21 +0200

    Time: 82 ms (93.10%), Space: 38.5 MB (34.48%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cs
+++ 1402-reducing-dishes/1402-reducing-dishes.cs
@@ -3,9 +3,6 @@ public class Solution {
         Array.Sort(satisfaction);
         int n= satisfaction.Length;
         int FirstPosativeIndex =-1;
-        foreach(int x in satisfaction)
-            Console.Write($" {x} ");
-        Console.WriteLine("");
         for(int i=0;i<n;i++)
         {
             if(satisfaction[i] >=0){
@@ -15,11 +12,9 @@ public class Solution {
         }
         if(FirstPosativeIndex ==-1)
             return 0;
-        Console.WriteLine($"First index = {FirstPosativeIndex}");
         int[] Posatives = satisfaction[FirstPosativeIndex..];
         int Sum = Posatives.Sum();
         int Power = Posatives.Select( (x,i) => x*(i+1) ).Sum();
-        Console.WriteLine($"SUm = {Sum} pow = {Power}");
         
         int[] Negatives =satisfaction[0..FirstPosativeIndex];
         int m = Negatives.Length;
@@ -28,7 +23,6 @@ public class Solution {
             int Minus = Negatives[i];
             int dif = Plus+Minus;
             if(dif<0) break;
-            Console.WriteLine($"Take {Minus} because Plus = {Plus} , Minus = {Minus}");
             Sum += Minus;
             Power += dif;
         }
