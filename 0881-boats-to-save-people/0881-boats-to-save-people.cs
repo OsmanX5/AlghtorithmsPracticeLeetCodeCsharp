6b62cf4517163ec3aacba5ab39e7357f6b8db3fb
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-04-03 18:34:53 +0200

    Time: 600 ms (5.75%), Space: 59.1 MB (8.05%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int NumRescueBoats(int[] people, int limit) {
+        Array.Sort(people);
+        Array.Reverse(people);
+        int n = people.Length;
+        int p1=0;
+        int p2=n-1;
+        int res=0;
+        while(p1<=p2){
+            int w1 = people[p1];
+            int w2 = people[p2];
+            Console.WriteLine("w1 = {0} w2 = {1}" , w1,w2);
+            if(w1+w2> limit){
+                p1++;
+                Console.WriteLine("Just take p1");
+            }
+            else{
+                p1++;
+                p2--;
+                Console.WriteLine("Just take both");
+            }
+            res+=1;
+            Console.WriteLine("End with p1 = {0} and p2 = {1} and res = {2}",p1,p2,res);
+
+        }
+        return res;
+        
+    }
+}
