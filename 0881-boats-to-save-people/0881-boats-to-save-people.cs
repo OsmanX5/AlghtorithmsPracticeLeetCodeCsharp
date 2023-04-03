5b2aefaa4b6da72a3f00c20a886d74e5d2cebddd
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-04-03 18:35:10 +0200

    Time: 159 ms (93.10%), Space: 49.3 MB (79.31%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cs
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cs
@@ -9,18 +9,18 @@ public class Solution {
         while(p1<=p2){
             int w1 = people[p1];
             int w2 = people[p2];
-            Console.WriteLine("w1 = {0} w2 = {1}" , w1,w2);
+            //Console.WriteLine("w1 = {0} w2 = {1}" , w1,w2);
             if(w1+w2> limit){
                 p1++;
-                Console.WriteLine("Just take p1");
+            //    Console.WriteLine("Just take p1");
             }
             else{
                 p1++;
                 p2--;
-                Console.WriteLine("Just take both");
+              //  Console.WriteLine("Just take both");
             }
             res+=1;
-            Console.WriteLine("End with p1 = {0} and p2 = {1} and res = {2}",p1,p2,res);
+          //  Console.WriteLine("End with p1 = {0} and p2 = {1} and res = {2}",p1,p2,res);
 
         }
         return res;
