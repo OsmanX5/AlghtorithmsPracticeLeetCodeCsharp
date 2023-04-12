d8ac3621291690008658e584e2e7b0fd4c23ea3c
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-04-12 07:19:55 +0200

    Time: 86 ms (45.41%), Space: 39.2 MB (47.45%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cs
+++ 0071-simplify-path/0071-simplify-path.cs
@@ -2,10 +2,9 @@ public class Solution {
     public string SimplifyPath(string path) {
         var Dirs  = path.Split('/');
         var st = new Stack<String>();
-        foreach(string dir in Dirs){
+        foreach(string dir in Dirs)
             if(dir=="..")st.TryPop(out _);
             else if(dir!="" && dir!=".") st.Push(dir);
-        }
         return "/"+string.Join("/",st.Reverse());
     }
 }
