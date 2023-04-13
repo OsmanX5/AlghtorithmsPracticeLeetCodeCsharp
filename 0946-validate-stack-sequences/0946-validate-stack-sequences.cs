e995329f5c5d79755fc0fc78b3759b5033c72d6a
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-04-13 09:55:00 +0200

    Time: 94 ms (88.46%), Space: 42 MB (92.31%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cs
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cs
@@ -5,14 +5,10 @@ public class Solution {
 		foreach (var p in pushed)
 		{
 			st.Push(p);
-			Console.WriteLine($"Push {p}");
-			
 			while (st.Count > 0 && st.Peek() == popped[PopIndex])
 			{
-				Console.WriteLine($"Pop{st.Peek()}");
 				st.Pop();
 				PopIndex++;
-
 			}
 		}
 		return st.Count == 0;
