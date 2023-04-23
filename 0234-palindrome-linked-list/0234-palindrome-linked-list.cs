41a132c644520c9af205ef2fc5fbef606bc2243c
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-04-23 14:58:40 +0200

    Time: 236 ms (85.50%), Space: 61.8 MB (59.77%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cs
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cs
@@ -13,12 +13,9 @@ public class Solution {
     public bool IsPalindrome(ListNode head) {
         ListNode middle = FindLinkedListMiddle(head);
         ListNode head2 = length%2==0 ? middle : middle.next;
-        Console.WriteLine("mid ={0}",middle.val);
         head = ReverseLinkedList(head,middle);
         ListNode temp1= head;
         ListNode temp2 =head2;
-        PrintLinkedList(head);
-        PrintLinkedList(head2);
         while(temp1!=null && temp2 !=null){
             if(temp1.val!=temp2.val) return false;
             temp1=temp1.next;
@@ -55,14 +52,5 @@ public class Solution {
 
         return prev;
     }
-    public void PrintLinkedList(ListNode head) {
-    ListNode current = head;
-    
-    while (current != null) {
-        Console.Write(current.val + " ");
-        current = current.next;
-    }
-    
-    Console.WriteLine();
-    }
+
 }
