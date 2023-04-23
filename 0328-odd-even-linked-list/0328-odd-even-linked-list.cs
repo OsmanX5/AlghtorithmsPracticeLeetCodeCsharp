fe2278933e5eddf9852069b5847d5d75a4c39ddd
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-04-23 14:17:10 +0200

    Time: 97 ms (16.71%), Space: 40.1 MB (36.49%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode OddEvenList(ListNode head) {
+        if(head==null)return head;
+        ListNode firstEven = head.next;
+        ListNode LastOdd = null;
+        ListNode temp = head;
+        while(temp!=null){
+            ListNode oddNode = temp;
+            ListNode EvenNode = temp.next;
+            if(EvenNode == null){
+                LastOdd = oddNode;
+                break;
+            }
+            ListNode NextOdd = EvenNode.next;
+            if(NextOdd==null){
+                LastOdd = oddNode;
+                break;
+            }
+            LastOdd = NextOdd;
+            oddNode.next = NextOdd;
+            ListNode NextEven = NextOdd.next;
+            EvenNode.next = NextEven;
+            temp = NextOdd;
+            
+        }
+        LastOdd.next = firstEven;
+        return head;
+    }
+}
