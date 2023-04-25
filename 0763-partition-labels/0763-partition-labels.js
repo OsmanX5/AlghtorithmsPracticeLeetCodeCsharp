6804035382f6b23d229260d66b25f1514a2b0e34
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-04-25 21:58:17 +0200

    Time: 78 ms (27.35%), Space: 45.5 MB (14.74%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.js
+++ 0763-partition-labels/0763-partition-labels.js
@@ -4,25 +4,16 @@
  */
 var partitionLabels = function(s) {
     let dict = new Map()
-    for(let i in s){
-        dict.set(s[i],i);
-    }
+    for(let i in s) dict.set(s[i],i);
     let CurrentIndex = 0;
-    let n = +s.length;
     let res =[]
-    let MaxIndex =1;
-    while(CurrentIndex<n){
+    while(CurrentIndex<+s.length){
         let startIndext = CurrentIndex;
-        MaxIndex = dict.get(s[CurrentIndex]);
-        //console.log("start a loop with ",startIndext);
-        while(CurrentIndex<MaxIndex){
-            let ThisCharMaxIndex = dict.get(s[CurrentIndex]);
-            MaxIndex = Math.max( MaxIndex , ThisCharMaxIndex );
-           // console.log(`now ${s[CurrentIndex]} and it is in ${ThisCharIndexes} so max Index = ${MaxIndex}`);
-            CurrentIndex++;
-        }
+        let MaxIndex = dict.get(s[CurrentIndex]);
+        while(CurrentIndex<MaxIndex)
+            MaxIndex = Math.max( MaxIndex , dict.get(s[CurrentIndex++]) );
         res.push(MaxIndex-startIndext+1);
         CurrentIndex+=1;
-}
+    }
     return res;
 };
