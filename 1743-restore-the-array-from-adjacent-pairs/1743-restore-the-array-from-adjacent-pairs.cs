e81c3ff293d3c8fcdb14c02846d6a3b3808925b6
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-11-10 12:52:49 +0300

    Time: 422 ms (35.71%), Space: 80.7 MB (85.71%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cs
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cs
@@ -5,25 +5,28 @@ public class Solution {
         
         //create adjList
         foreach(var pair in adjacentPairs){
-            int x = pair[0];
-            int y = pair[1];
+            (int x ,int y ) = (pair[0] , pair[1]);
             if(!adjList.ContainsKey(x)) adjList[x] = new List<int>();
             if(!adjList.ContainsKey(y)) adjList[y] = new List<int>();
             adjList[x].Add(y);
             adjList[y].Add(x);
             
         }
-        foreach(var dic in adjList) if(dic.Value.Count ==1) head = dic.Key;
         int n = adjList.Keys.ToList().Count;
+
+        foreach(var dic in adjList) if(dic.Value.Count ==1) 
+            head = dic.Key;
+        int cur = adjList[head][0];
+
         int[] result = new int[n];
         result[0] = head;
-        result[1] = adjList[head][0];
-        int cur = result[1];
+        result[1] = cur;
+        
         var visited = new HashSet<int>(){head , cur};
-       for(int i=2;i<n;i++){
+        for(int i=2;i<n;i++){
             var pairs = adjList[cur];
             int nextItem = pairs[0];
-            if(visited.Contains(nextItem)) 
+            if(result[i-2] == nextItem) 
                 nextItem=  pairs[1] ;
             result[i] = nextItem;
             visited.Add(nextItem);
