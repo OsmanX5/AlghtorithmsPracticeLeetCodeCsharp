af104ccfd967f7f732df40651808768668dfcd4a
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-12-10 13:22:00 +0300

    Time: 149 ms (15.79%), Space: 47.7 MB (43.86%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cs
+++ 0867-transpose-matrix/0867-transpose-matrix.cs
@@ -5,25 +5,13 @@ public class Solution {
         int[][] res = new int[m][];
         for(int i=0;i<m;i++)
             res[i] = new int[n];
-        printArray(matrix);
-        printArray(res);
         for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
                 res[j][i] = matrix[i][j];
         return res;
         
     }
-    void printArray(int[][] arr){
-        int n = arr.Length;
-        int m = arr[0].Length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++)
-            {
-                Console.Write($"  {arr[i][j]}  ");
-            }
-            Console.WriteLine("");
-        }
-            
+  
  
-    }
+    
 }
