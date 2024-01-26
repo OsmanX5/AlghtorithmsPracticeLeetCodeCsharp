753b396010b4dcd3f58a49e224568bda0e1aa218
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2024-01-26 21:57:26 +0300

    Time: 53 ms (94.87%), Space: 58.5 MB (57.81%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cs
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cs
@@ -1,28 +1,19 @@
 public class Solution {
     public bool RepeatedSubstringPattern(string s) {
-        string repated = $"{s[0]}";
+        string repated = "";
         int n = s.Length;
-        int p = 0;
-        if(repated.Length == n )
-                return false;
         for(int len =1;len<n/2+1;len++){
-            if(n%len !=0 ){
-                Console.WriteLine($"len = {len} , n%len = {n%len}");
+            if(n%len !=0 )
                 continue;
-            }
             repated = s.Substring(0,len);
-            Console.WriteLine($"Repated test = {repated}");
             int times = 0;
             for(int i=0;i<n;i+=len)
             {
-                if(repated == s.Substring(i,len)){
+                if(repated == s.Substring(i,len))
                     times++;
-                }
-                    
                 else
                     break;
             }
-            Console.WriteLine($"{repated} repated for {times}");
             if(times == n/len)
                 return true;
         }
