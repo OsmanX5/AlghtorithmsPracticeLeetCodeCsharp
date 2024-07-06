857c7d4f61d15dbb125cb6c978c5690eb824e169
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2024-07-06 14:12:09 +0300

    Time: 322 ms (5.84%), Space: 54 MB (7.30%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cs
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cs
@@ -1,9 +1,6 @@
 public class Solution {
     public int NumberOfBeams(string[] bank) {
-        int res=0;
         int[] devices = bank.Select(row => row.Where(c => c == '1').Count()).Where(x => x!=0 ).ToArray();
-        for (int i=0;i<devices.Length-1;i++)
-            res += devices[i] *devices[i+1];
-        return res;
+        return devices.Zip(devices.Skip(1), (num1 , num2) => num1 * num2 ).Sum();
     }
 }
