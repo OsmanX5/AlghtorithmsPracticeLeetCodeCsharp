a1098fdb83abeab481fc171966193a2218bbd218
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2025-12-06 08:40:06 +0300

    Time: 2 ms (8.90%) | Memory: 56.5 MB (5.26%) - LeetSync

---
+++ 290-word-pattern/word-pattern.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let patDict = new Map();
+    let words = s.split(" ");
+    if(words.length!=pattern.length){
+        console.log('here 3')
+        return false;
+    }
+    for(let i=0;i<pattern.length;i++)
+    {
+        let p = pattern[i];
+        let word = words[i]
+        if(patDict.has(p) && patDict.get(p) != word)
+            return false;
+        else{
+            for(let [key, value] of patDict){
+                if(key !=p && value ==word){
+                    console.log('here2')
+                    return false;
+                }
+            }
+            patDict.set(p,word)
+        }
+    }
+
+    return true
+};
